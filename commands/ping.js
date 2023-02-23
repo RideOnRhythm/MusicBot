3d5b4ab77aba33193a5e998ed346df9a588b3936
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-02-23 16:44:58 +0800

    fixed ping command

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,13 @@ exports.run = async (client, message, args) => {
     if (!node) return;
     const player = node.players.get(message.guild.id);
 
-    await message.channel.send(`Bot Ping: ${Math.round(client.ws.ping)}ms\nNode Ping: ${Math.round(player.ping)}`);
+    let nodeString = '';
+    if (player === undefined) {
+        nodeString = 'Not connected.';
+    } else {
+        nodeString = Math.round(player.ping).toString();
+    }
+    await message.channel.send(`Bot Ping: ${Math.round(client.ws.ping)}ms\nNode Ping: ${nodeString}ms`);
 };
 
 exports.name = 'ping';
